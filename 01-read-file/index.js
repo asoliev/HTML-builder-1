b669dddb50a1c091a7be1d6286982cf6d8e5cc6b
asoliev <91322310+asoliev@users.noreply.github.com>
2023-05-09 14:18:24 +0500

    refactor 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-
-const fileName = 'text.txt';
-const filePath = path.join(__dirname, fileName);
-
-let readableStream = fs.createReadStream(filePath, "utf8");
-
-readableStream.on("data", function(chunk){ 
-    console.log(chunk);
-});
+const { stdout } = require('process');
+const input = fs.createReadStream(path.join(__dirname, 'text.txt'), "utf8");
+input.on("data", chunk => stdout.write(chunk));
